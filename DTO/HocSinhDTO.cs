8600865573017e35c3763286696ac8252c2865a6
quanghieu2004 <104124236+quanghieu2004@users.noreply.github.com>
2024-05-13 14:17:35 +0700

    Add files via upload

---
+++ DTO/HocSinhDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DTO
+{
+    public class HocSinhDTO
+    {
+        private string maHocSinh;
+        private string hoTen;
+        bool gioiTinh;
+        private string diaChi;
+        private string maDanToc;
+        private string maTonGiao;
+
+        public HocSinhDTO() { }
+        public HocSinhDTO(
+            string maHocSinh,
+            string hoTen,
+            bool gioiTinh,
+            string diaChi,
+            string maDanToc,
+            string maTonGiao)
+        {
+            this.maHocSinh = maHocSinh;
+            this.hoTen = hoTen;
+            this.gioiTinh = gioiTinh;
+            this.diaChi = diaChi;
+            this.maDanToc = maDanToc;
+            this.maTonGiao = maTonGiao;
+        }
+
+        public string MaHocSinh { get => maHocSinh; set => maHocSinh = value; }
+        public string HoTen { get => hoTen; set => hoTen = value; }
+        public bool GioiTinh { get => gioiTinh; set => gioiTinh = value; }
+        public string DiaChi { get => diaChi; set => diaChi = value; }
+        public string MaDanToc { get => maDanToc; set => maDanToc = value; }
+        public string MaTonGiao { get => maTonGiao; set => maTonGiao = value; }
+    }
+}
---
+++ DTO/LopDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DTO
+{
+    public class LopDTO
+    {
+        private string maLop;
+        private string tenLop;
+        private string maKhoiLop;
+        private string maNamHoc;
+        private string siSo;
+        private string maGiaoVien;
+
+        public LopDTO() { }
+        public LopDTO(string maLop, string tenLop, string maKhoiLop, string maNamHoc, string siSo, string maGiaoVien)
+        {
+            this.maLop = maLop;
+            this.tenLop = tenLop;
+            this.maKhoiLop = maKhoiLop;
+            this.maNamHoc = maNamHoc;
+            this.siSo = siSo;
+            this.maGiaoVien = maGiaoVien;
+        }
+
+        public string MaLop { get => maLop; set => maLop = value; }
+        public string TenLop { get => tenLop; set => tenLop = value; }
+        public string MaKhoiLop { get => maKhoiLop; set => maKhoiLop = value; }
+        public string MaNamHoc { get => maNamHoc; set => maNamHoc = value; }
+        public string SiSo { get => siSo; set => siSo = value; }
+        public string MaGiaoVien { get => maGiaoVien; set => maGiaoVien = value; }
+    }
+}
